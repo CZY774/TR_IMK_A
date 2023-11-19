5e7394773a45acc1e293c8d1703350e230a749b2
CZY774 <118541672+CZY774@users.noreply.github.com>
2023-11-19 21:53:36 +0700

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-// Fungsi untuk memperbarui tampilan berdasarkan status dark mode
+// Fungsi untuk menentukan apakah dark mode aktif atau tidak
+function isDarkMode() {
+  // Periksa local storage, jika belum ada, kembalikan false
+  return localStorage.getItem('darkMode') === 'true';
+}
+
+// Fungsi untuk memperbarui tema halaman berdasarkan status dark mode
 function updateTheme() {
-    const isDark = isDarkMode();
-  
-    // Toggle class pada body
-    document.body.classList.toggle('dark-mode', isDark);
-  
-    // Toggle class pada sidebar
-    const sidebar = document.getElementById('mySidebar');
-    if (sidebar) {
-      sidebar.classList.toggle('dark-mode', isDark);
-    }
-  
-    // Toggle class pada elemen yang memiliki kelas .w3-theme-l5
-    const themeElements = document.querySelectorAll('.w3-theme-l5');
-    themeElements.forEach(element => {
-      element.classList.toggle('dark-mode', isDark);
-    });
-  
-    // Ganti warna background dan font
-    document.body.style.backgroundColor = isDark ? '#000' : '#fff';
-    document.body.style.color = isDark ? '#fff' : '#000';
-  
-    // Reset icon dark mode
-    const darkModeIcon = document.getElementById('dark-mode-icon');
-    darkModeIcon.style.transform = isDark ? 'scaleX(-1)' : 'scaleX(1)';
-  }
-  
-  // Panggil updateTheme saat page load
-  window.onload = updateTheme;
+  const isDark = isDarkMode();
+  const darkModeIcon = document.getElementById('dark-mode-icon');
+
+  // Ganti warna latar belakang dan teks sesuai dengan mode
+  document.body.style.backgroundColor = isDark ? '#000' : '#fff';
+  document.body.style.color = isDark ? '#fff' : '#000';
+
+  // Menerapkan transformasi untuk membalikkan ikon saat dark mode aktif
+  darkModeIcon.style.transform = isDark ? 'scaleX(-1)' : 'scaleX(1)';
+}
+
+// Fungsi untuk mengubah status dark mode dan memperbarui tampilan
+function toggleDarkMode() {
+  const darkModeIcon = document.getElementById('dark-mode-icon');
+  const isDark = isDarkMode();
+
+  // Toggle status dark mode
+  localStorage.setItem('darkMode', !isDark);
+
+  // Panggil fungsi untuk memperbarui tema halaman
+  updateTheme();
+}
+
+// Panggil fungsi untuk memperbarui tema saat halaman dimuat
+document.addEventListener('DOMContentLoaded', function() {
+  updateTheme();
+});
+
+// Setelah halaman dimuat, panggil fungsi untuk memeriksa status dark mode
+// dan memperbarui tema secara otomatis
+updateTheme();
