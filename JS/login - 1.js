1dfdf651d1052e0db7faae5613a95135d7706e5d
CZY774 <118541672+CZY774@users.noreply.github.com>
2023-12-02 13:48:48 +0700

    Update login - 1.js

--- JS/login - 1.js
+++ JS/login - 1.js
@@ -8,4 +8,43 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+});// pilih element form
+const form = document.getElementById('form');
+
+// pilih semua input field di dalam form
+const inputs = form.querySelectorAll('input');
+
+// event listener untuk submit form
+form.addEventListener('submit', (e) => {
+
+  // cegah submit default
+  e.preventDefault();
+  
+  // reset state validasi
+  let isFormValid = true;
+
+  // lakukan loop through semua input 
+  inputs.forEach(input => {
+  	// jika ada yang kosong
+    if(!input.value) {
+      // ubah state menjadi false
+      isFormValid = false;
+    }
+  });
+
+  // jika semua input terisi
+  if(isFormValid) {
+  	// panggil fungsi untuk submit data
+    formSubmit();
+  }
+
 });
+
+// fungsi untuk submit form
+function formSubmit() {
+  // proses ajax dsb
+  console.log('mengirim data'); 
+
+  // redirect halaman
+  window.location.href = '/dashboard'; 
+}
